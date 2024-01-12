e29350a3ca88ca53611ad41e1a7dc8b04ed72de1
anyel-ec <paulangel826@gmail.com>
2024-01-12 14:24:41 -0500

    upload the class Demo, data in  array

--- src/main/java/org/espe/prueba/Demo.java
+++ src/main/java/org/espe/prueba/Demo.java
@@ -3,24 +3,39 @@ package org.espe.prueba;
 import org.espe.modelo.ClienteBean;
 import org.espe.vista.ClienteVista;
 import org.espe.controlador.ClienteControlador;
+
 public class Demo {
     public static void main(String[] args) {
-        // objeto vista y modelo
-        ClienteBean oModelo = llenarDatosCliente();
+        // Objeto vista y modelo para el primer cliente
+        ClienteBean primerCliente = llenarDatosCliente(1, "Angel",
+                "Patiño", "22222222222");
         ClienteVista oVista = new ClienteVista();
+        ClienteControlador oControlador = new ClienteControlador(oVista, primerCliente);
 
-        // objeto controlador y pasamos la vista y el modleo
-        ClienteControlador oControlador = new ClienteControlador(oVista, oModelo);
+        // Actualizar vista con los datos del primer cliente
         oControlador.actualizarVista();
 
+        // Crear un arreglo con los datos de dos clientes adicionales
+        ClienteBean[] clientes = new ClienteBean[2];
+        clientes[0] = llenarDatosCliente(2, "Eva",
+                "Gomez", "33333333333");
+        clientes[1] = llenarDatosCliente(3, "Juan",
+                "Lopez", "44444444444");
+
+        // Actualizar la vista con los datos de los dos clientes adicionales
+        for (ClienteBean cliente : clientes) {
+            oControlador = new ClienteControlador(oVista, cliente);
+            oControlador.actualizarVista();
+        }
     }
-    // se muestra los datos del cliente
-    private static ClienteBean llenarDatosCliente() {
+
+    // Método para llenar los datos de un cliente
+    private static ClienteBean llenarDatosCliente(int id, String nombre, String apellido, String cedula) {
         ClienteBean cliente = new ClienteBean();
-        cliente.setIdCli(1);
-        cliente.setNombreCli("Angel");
-        cliente.setApellidoCli("Patiño");
-        cliente.setCedulaCli("22222222222");
+        cliente.setIdCli(id);
+        cliente.setNombreCli(nombre);
+        cliente.setApellidoCli(apellido);
+        cliente.setCedulaCli(cedula);
         return cliente;
     }
 }
